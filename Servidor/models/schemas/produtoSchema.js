568be8642d1529e810860ff05818dc9e8abe416f
Adriano dos Anjos <adferreiraneto@gmail.com>
2022-06-21 19:47:56 -0300

    todo projeto

---
+++ Servidor/models/schemas/produtoSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const produtoSchema = new mongoose.Schema({
+    
+    codigo: Number,
+    nome: String,
+    desc: String,
+    cor: String,
+    valor: String,
+    foto: String,
+    categoria: {type: mongoose.Schema.Types.ObjectId, ref: 'categoria'},
+    //categoria: String,
+    quantidade: Number,
+});
+
+module.exports = produtoSchema;
